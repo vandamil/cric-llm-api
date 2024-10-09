ee751a988a42298a87cce2736bb13c00b4c6f21d
vandamil <vandamilvandamil488@gmail.com>
2024-10-09 23:02:37 +0530

    env

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from vanna.openai import OpenAI_Chat
 from openai import OpenAI
 from fastapi import FastAPI
 from vanna.vannadb import VannaDB_VectorStore
+import os
+
+MY_VANNA_MODEL = os.environ["MY_VANNA_MODEL"]
+vanna_api_key = os.environ["vanna_api_key"]
+api_key = os.environ["api_key"]
+
+
 
 class MyVanna(VannaDB_VectorStore, OpenAI_Chat):
     def __init__(self, config=None):
-        MY_VANNA_MODEL = 'sel_1'
-        VannaDB_VectorStore.__init__(self, vanna_model=MY_VANNA_MODEL, vanna_api_key='390bc303830b4105b5a27b0e9517434a', config=config)
-        OpenAI_Chat.__init__(self, client=OpenAI(base_url="https://api.groq.com/openai/v1",api_key='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'), config=config) # Make sure to put your AzureOpenAI client here
+        VannaDB_VectorStore.__init__(self, vanna_model=MY_VANNA_MODEL, vanna_api_key=vanna_api_key, config=config)
+        OpenAI_Chat.__init__(self, client=OpenAI(base_url="https://api.groq.com/openai/v1",api_key=api_key), config=config) # Make sure to put your AzureOpenAI client here
 
 vn = MyVanna(config={'model': "llama3-8b-8192"})
 
